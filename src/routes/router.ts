f8afcd7a0e3d19c7fb68df82b6a214e760cd199a
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-09 01:12:21 -0500

    Acoplando los comandos para el deploy 9

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,25 +1,28 @@
 // routes/index.ts
 import { Router } from "express";
+import { directivoLoginRouter } from "./api/login/directivo";
 
-import path from "path";
-import findAllIndexTsFiles from "../lib/helpers/functions/findAllIndexTsFiles";
+// import path from "path";
+// import findAllIndexTsFiles from "../lib/helpers/functions/findAllIndexTsFiles";
 
 const router = Router();
-const PATH_ROUTER = __dirname;
+// const PATH_ROUTER = __dirname;
 
 // Encontrar todos los archivos index.ts
-const indexFiles = findAllIndexTsFiles(PATH_ROUTER);
+// const indexFiles = findAllIndexTsFiles(PATH_ROUTER);
 
-// Ahora puedes usar los resultados para crear tus rutas como prefieras
-indexFiles.forEach((file) => {
+// // Ahora puedes usar los resultados para crear tus rutas como prefieras
+// indexFiles.forEach((file) => {
 
-  const routeParts = path.relative(PATH_ROUTER, file.dirPath).split(path.sep);
-  const endpointPath = "/" + routeParts.join("/");
+//   const routeParts = path.relative(PATH_ROUTER, file.dirPath).split(path.sep);
+//   const endpointPath = "/" + routeParts.join("/");
 
-  //Importando dinamicamente los routers de los archivos index.ts de cualquier nivel dentro de la carpeta routes 
-  import("./" + file.relativePath).then((module) => {
-    router.use(endpointPath, module.router);
-  });
-});
+//   //Importando dinamicamente los routers de los archivos index.ts de cualquier nivel dentro de la carpeta routes
+//   import("./" + file.relativePath).then((module) => {
+//     router.use(endpointPath, module.router);
+//   });
+// });
+
+router.use("/api/login/directivo", directivoLoginRouter);
 
 export default router;
