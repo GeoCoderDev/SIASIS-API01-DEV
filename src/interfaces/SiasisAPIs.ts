13680d208d3ed8f7d9925e5e32caaf45ae6a3c6e
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-09 02:23:37 -0500

    Se corrigio la interfaz de respuesta de Lgin Exitoso

--- src/interfaces/SiasisAPIs.ts
+++ src/interfaces/SiasisAPIs.ts
@@ -1,3 +1,16 @@
+import { RolesSistema } from "./RolesSistema";
+
 export interface ApiResponseBase {
-    message: string;
+  message: string;
 }
+
+export interface SuccessLoginData {
+  Nombres: string;
+  Apellidos: string;
+  Rol: RolesSistema;
+  token: string;
+}
+
+export type ResponseSuccessLogin = ApiResponseBase & {
+  data: SuccessLoginData;
+};
