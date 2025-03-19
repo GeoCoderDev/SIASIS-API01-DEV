d4cfd6807ff42e7020c6b73d036c47f9e912f228
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-19 00:46:37 -0500

    Actualizacion de tipos para el endpoint de actualizacion de foto de perfil

--- src/routes/api/directivo/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import directivoFotoPerfilRouter from "./foto-perfil";
-
-const router = Router();
-
-router.use("/foto-perfil", directivoFotoPerfilRouter);
-
-export default router;
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,7 @@
 // routes/index.ts
 import { Router } from "express";
 import loginRouter from "./api/login";
-<<<<<<< HEAD
 
-=======
-import directivoRouter from "./api/directivo";
->>>>>>> e45d1bcaed99d651f98de1b356752afde81b9965
 import misDatosRouter from "./api/mis-datos";
 import { UserAuthenticatedAPI01 } from "../interfaces/JWTPayload";
 import AllErrorTypes from "../interfaces/shared/apis/errors";
@@ -29,10 +25,6 @@ declare global {
 }
 
 router.use("/login", loginRouter);
-<<<<<<< HEAD
-=======
-router.use("/directivo", directivoRouter);
->>>>>>> e45d1bcaed99d651f98de1b356752afde81b9965
 router.use("/mis-datos", misDatosRouter);
 
 export default router;
