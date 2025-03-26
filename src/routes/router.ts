c06aec60abb17c1a32ab4e363f76b9431d95b09a
Juanito3500 <juanchavezsaldana1@gmail.com>
2025-03-26 00:15:44 -0500

    Endpoint de AUXILIARES y PERSONAL Administrativo

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,17 @@
 // routes/index.ts
 import { Router } from "express";
-import loginRouter from "./api/login";
 
-import misDatosRouter from "./api/mis-datos";
 import { UserAuthenticatedAPI01 } from "../interfaces/JWTPayload";
 import AllErrorTypes from "../interfaces/shared/apis/errors";
 import { ErrorDetails } from "../interfaces/shared/apis/errors/details";
+import isDirectivoAuthenticated from "../middlewares/isDirectivoAuthenticated";
+import checkAuthentication from "../middlewares/checkAuthentication";
+
+import loginRouter from "./api/login";
+import misDatosRouter from "./api/mis-datos";
+import auxiliaresRouter from "./api/auxiliares";
+import personalAdministrativoRouter from "./api/personal-administrativo";
+
 const router = Router();
 
 // Extender la interfaz Request de Express
@@ -26,5 +32,20 @@ declare global {
 
 router.use("/login", loginRouter);
 router.use("/mis-datos", misDatosRouter);
+router.use(
+  "/auxiliares",
+  isDirectivoAuthenticated as any,
+  checkAuthentication as any,
+  auxiliaresRouter
+);
+
+router.use("/login", loginRouter);
+router.use("/mis-datos", misDatosRouter);
+router.use(
+  "/personal-administrativo",
+  isDirectivoAuthenticated as any,
+  checkAuthentication as any,
+  personalAdministrativoRouter
+);
 
 export default router;
